d007ca1b76746ab26a46f2f02a2141008fe29889
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-18 10:11:26 +0530

    Add files via upload

---
+++ 0.1 Format Specifiers/int_chat_float_calling_into_Print_function.c
@@ -0,0 +1,15 @@
+//To print other types, use %c for char and %f for float: 
+#include <stdio.h>
+int main() 
+{
+  // Create variables
+  int myNum = 20;              // Integer (whole number)
+  float myFloatNum = 20.99;     // Floating point number
+  char myLetter = 'M';         // Character
+  
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
