f1a1c11e85b025be3e808e67b218d731fe1a7038
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-21 18:13:06 +0530

    Add files via upload

---
+++ 7.conditional statements/1. If statement.c
@@ -0,0 +1,19 @@
+/*If statement*/
+#include<stdio.h>
+int main()
+{
+	int a=50,b=80;
+	if(a>b){
+		printf("\n A is Big and ");
+	}
+	if(a<b){
+		printf("\n B is Big and");
+	}
+	if(a==b){
+		printf("\n A Equal to B");
+	}
+	if(a!=b){
+		printf("\n A Not Equla to  B");
+	}
+	return 0;
+}
