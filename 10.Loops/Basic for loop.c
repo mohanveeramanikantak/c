e68313141a0e6a5a32a81c7f92ad396d77c8bce4
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-29 10:47:35 +0530

    Add files via upload

---
+++ 10.Loops/Basic for loop.c
@@ -0,0 +1,11 @@
+//for loop
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=0; i<5; i++)
+	{
+		printf("The Value is i is :%d\n",i);
+	}
+	return 0;
+}
---
+++ 10.Loops/Basic for loop_Mohan.c
@@ -0,0 +1,12 @@
+/*For Loop in Scanf Model*/
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("Enter a Number : ");
+	scanf("%d",&n);
+	for(i=0; i<n; i++)
+	{
+		printf("\n Mohan");
+	}
+}
